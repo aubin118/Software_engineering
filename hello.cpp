c6186c681852c5a119b45afb9c6ce8979bde4349
aubin118 <aubin.coutard@outlook.com>
2024-09-16 15:28:15 +0200

    gestion versions

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include<HelloProjConfig.h>
 
-void hello_world() {
+void hello_world(){
 std::cout << "Hello World!\n";
 }
+
+void print_version(){
+std::cout << "project version is " << HelloProj_VERSION_MAJOR << "." << HelloProj_VERSION_MINOR << std::endl;
+}
+
+
 int main()
 {
 	hello_world();
+	print_version();
 }
+
